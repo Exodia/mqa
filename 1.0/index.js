3f45855a5d226177bf20735acb60aa5b8a18fd1e
Exodia <d_xinxin@163.com>
2013-05-05 00:13:32 +0800

    update

--- 1.0/index.js
+++ 1.0/index.js
@@ -1,6 +1,15 @@
+/**
+ * @fileoverview 媒体查询假名组件
+ * @author 踏风<tafeng.dxx@taobao.com>
+ * @module mqa
+ **/
 ;
 KISSY.add(function (S, Mqa) {
-    return Mqa
+
+    return Mqa;
 }, {
-    requires: ['./mqa']
+    requires: ["./mqa.js"]
 })
+
+
+
